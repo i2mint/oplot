3f8a6277035651ab526c00de98256a4104ba8e8c
Thor Whalen <thorwhalen1@gmail.com>
2024-09-05 13:42:26 +0200

    chore: bring matrix plotting functions to root

--- oplot/__init__.py
+++ oplot/__init__.py
@@ -5,4 +5,14 @@ from oplot.ui_scores_mapping import make_ui_score_mapping
 from oplot.outlier_scores import plot_scores_and_zones
 from oplot.plot_data_set import scatter_and_color_according_to_y, side_by_side_bar
 from oplot.plot_stats import plot_confusion_matrix
-from oplot.matrix import heatmap
+from oplot.matrix import (
+    heatmap,
+    heatmap_sns,
+    xy_boxplot,
+    vlines_ranges,
+    vlines_of_matrix,
+    labeled_heatmap,
+    get_figsize_to_fit,
+    plot_simil_mat_with_labels,
+    hierarchical_cluster_sorted_heatmap,
+)
